31d3598e91cff7e9a65c2b309cf3a0328b85f761
Andrii Chipets <andrii.chipets@gmail.com>
2024-04-23 09:05:36 +0300

    solved problem with sql scripts initializing before @PostController execution by adding liquibase configuration to application-test.yaml (changeLog: classpath:db/changelog-test.sql). Only 9 tests fails, sql scripts are needed to be refactored

--- src/test/java/com/javarush/jira/AbstractControllerTest.java
+++ src/test/java/com/javarush/jira/AbstractControllerTest.java
@@ -14,7 +14,7 @@ import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilde
         config = @SqlConfig(encoding = "UTF-8"))
 @AutoConfigureMockMvc
 //https://docs.spring.io/spring-boot/docs/current/reference/html/spring-boot-features.html#boot-features-testing-spring-boot-applications-testing-with-mock-environment
-public abstract class AbstractControllerTest extends BaseTests {
+public abstract class AbstractControllerTest extends BaseTests  {
 
     @Autowired
     private MockMvc mockMvc;
